1f4c310e5d92797d7bf1253f86e24ed66d9eae99
haseeb <haseeeb8j@gmail.com>
2026-06-14 18:20:40 +0500

    updated: terminal interface now has colors

--- mono/interface/terminal.py
+++ mono/interface/terminal.py
@@ -1,6 +1,37 @@
 from mono.interface.base import BaseInterface
 
 
+class Color:
+
+	reset = "\033[0m"
+
+	black   = "\033[30m"
+	red     = "\033[31m"
+	green   = "\033[32m"
+	yellow  = "\033[33m"
+	blue    = "\033[34m"
+	magenta = "\033[35m"
+	cyan    = "\033[36m"
+	white   = "\033[37m"
+
+	bright_black   = "\033[90m"
+	bright_red     = "\033[91m"
+	bright_green   = "\033[92m"
+	bright_yellow  = "\033[93m"
+	bright_blue    = "\033[94m"
+	bright_magenta = "\033[95m"
+	bright_cyan    = "\033[96m"
+	bright_white   = "\033[97m"
+
+	@classmethod
+	def print(cls, msg: str, color: str, *, end="\n"):
+		print(f"{color}{msg}{Color.reset}", end=end)
+
+	@staticmethod
+	def colorify(text:str, color: str):
+		return f"{color}{text}{Color.reset}"
+
+
 class TerminalInterface(BaseInterface):
 
 
@@ -9,29 +40,31 @@ class TerminalInterface(BaseInterface):
 
 
 	def start(self):
-		print("[ Mono ] > Running terminal interface.")
+		Color.print("[ Mono ] > Running terminal interface.", Color.magenta)
 	
 
 	def end(self):
-		print("[ Mono ] > Ending interface.")
+		Color.print("[ Mono ] > Ending interface.", Color.magenta)
 
 
 	def listen(self) -> str | None:
 		try:
-			return input("> ").strip()
+			return input(">> ").strip()
 		except KeyboardInterrupt:
 			print()
 			return None
 
 
 	def tell(self, source: str, msg: str) -> None:
-		print(f"[{source}]: {msg}")
+		Color.print(f"[{source}]: {msg}", Color.blue)
 
 
 	def ask(self, source: str, msg: str, options: tuple[str, ...]) -> str:
-		full_prompt = f"[{source}]: {msg} ({'/'.join(options)}): "
+		full_prompt = f"[{source}]: {msg} ({'/'.join(options)})"
+		Color.print(full_prompt, Color.yellow)
+
 		while True:
-			choice = input(full_prompt).strip()
+			choice = input(">> ").strip()
 			if choice in options:
 				return choice
-			print(f"Invalid choice. Please choose from: {', '.join(options)}")
+			Color.print(f"Invalid choice. Please choose from: {', '.join(options)}", Color.red)
